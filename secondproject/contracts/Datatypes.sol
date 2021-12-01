fc6c01fd79f9ecaec707e671a591e5692490f30c
ashok <ashok@supersub.app>
2021-12-01 17:36:40 +0530

    Flow Control spaces removed

---
+++ secondproject/contracts/Datatypes.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+// SPDX-License-Identifier: MIT
+
+contract DataTypes {
+
+    uint x = 9;
+    int i = -68; 
+    uint8 j = 17;
+    bool isEthereumCool = true; 
+    address owner = msg.sender; // msg.sender is the Ethereum address of the account that sent this transaction
+    bytes32 bMsg = "hello";
+    string sMsg = "hello"; 
+
+    function getStateVariables() public view returns (uint, int, uint, bool, address, bytes32, string memory) {
+        return (x, i, j, isEthereumCool, owner, bMsg, sMsg); 
+    }
+
+}
